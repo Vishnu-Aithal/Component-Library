bda6855475a30793d55fd807ea36999504c4aee4
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2022-02-09 17:31:15 +0530

    Add - Top Nav and Side Bar components

--- navigation/navigation.js
+++ navigation/navigation.js
@@ -1,4 +1,23 @@
 document.querySelector(".nav-toggle").onclick = (e) => {
-  const toggleTarget = document.querySelector(e.target.dataset.toggle)
+  const toggleBtn = e.target;
+  const toggleTarget = document.querySelector(toggleBtn.dataset.toggle)
+  toggleBtn.classList.toggle("active")
   toggleTarget.classList.toggle("show")
+}
+
+document.querySelector(".side-bar-toggle").onclick = (e) => {
+  const toggleBtn = e.target;
+  const toggleTarget = document.querySelector(toggleBtn.dataset.toggle)
+  toggleTarget.classList.toggle("hide")
+  toggleTarget.classList.contains("hide") ?
+    toggleBtn.classList.remove("active") :
+    toggleBtn.classList.add("active");
+}
+
+document.querySelector(".list-sub-toggle").onclick = (e) => {
+  const toggleBtn = e.target;
+  // console.log(toggleBtn.dataset.toggle)
+  const toggleTarget = document.querySelector(toggleBtn.dataset.toggle)
+  toggleBtn.classList.toggle("active")
+  toggleTarget.classList.toggle("hide")
 }
